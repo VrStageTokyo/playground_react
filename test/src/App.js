e48d6fdea6681f14ebc4cd33e9cb33ef4d56dbc3
VrStageTokyo <soul.live.180@gmail.com>
2021-04-03 16:47:51 +0900

    creat_new_script

---
+++ test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './compornents/Counter'
+
+const App=()=>{
+    return (
+        <div className = 'ui container' style={{marginTop: '10px'}}>
+            <Counter/>
+        </div>
+
+
+    );
+};
+
+export default App
